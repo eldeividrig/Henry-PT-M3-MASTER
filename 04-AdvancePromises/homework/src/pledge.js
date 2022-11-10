10674a560f8d8589ef827302fd587cc3e1f465b5
David Gabriel Barboza <davidgabrielb@hotmail.com>
2022-11-10 00:13:54 -0300

    promises advanced finish

--- 04-AdvancePromises/homework/src/pledge.js
+++ 04-AdvancePromises/homework/src/pledge.js
@@ -11,43 +11,71 @@ function $Promise(executor) {
   executor(this._internalResolve.bind(this), this._internalReject.bind(this));//es lo mismo que la linea anterior solo que usa el bind en ves de fn flecha
 };
 
-$Promise.prototype._internalResolve = function(value){
-  if(this._state === 'pending') {
+$Promise.prototype._internalResolve = function (value) {
+  if (this._state === 'pending') {
     this._state = 'fulfilled';
     this._value = value;
     this._callHandlers();
   }
 };
 
-$Promise.prototype._internalReject = function(value){
-  if(this._state === 'pending') {
+$Promise.prototype._internalReject = function (value) {
+  if (this._state === 'pending') {
     this._state = 'rejected';
     this._value = value;
     this._callHandlers();
   }
 };
 
-$Promise.prototype.then = function(successCb, errorCb){
-  if(typeof successCb !== 'function') successCb = false;
-  if(typeof errorCb !== 'function') errorCb = false;
-  const downstreamPromise = new $Promise(function(){});
-  this._handlerGroups.push({successCb, errorCb, downstreamPromise});
-  if(this._state !== 'pending') this._callHandlers();
+$Promise.prototype.then = function (successCb, errorCb) {
+  if (typeof successCb !== 'function') successCb = false;
+  if (typeof errorCb !== 'function') errorCb = false;
+  const downstreamPromise = new $Promise(function () { });
+  this._handlerGroups.push({ successCb, errorCb, downstreamPromise });
+  if (this._state !== 'pending') this._callHandlers();
   return downstreamPromise;
 };
 
-$Promise.prototype._callHandlers = function(){
-  while(this._handlerGroups.length > 0){
-    let current =  this._handlerGroups.shift();
-    if(this._state === 'fulfilled'){
-      current.successCb && current.successCb(this._value);
-    }else if( this._state === 'rejected'){
-      current.errorCb && current.errorCb(this._value);
+$Promise.prototype._callHandlers = function () {
+  while (this._handlerGroups.length > 0) {
+    let current = this._handlerGroups.shift();
+    if (this._state === 'fulfilled') {
+      if (!current.successCb) {
+        current.downstreamPromise._internalResolve(this._value);
+      } else {
+        try {
+          const result = current.successCb(this._value);
+          if (result instanceof $Promise) {
+            result.then((value) => current.downstreamPromise._internalResolve(value),
+              (err) => current.downstreamPromise._internalReject(err));
+          } else {
+            current.downstreamPromise._internalResolve(result);
+          }
+        } catch (e) {
+          current.downstreamPromise._internalReject(e);
+        }
+      }
+    } else if (this._state === 'rejected') {
+      if (!current.errorCb) {
+        current.downstreamPromise._internalReject(this._value);
+      } else {
+        try {
+          const result = current.errorCb(this._value);
+          if (result instanceof $Promise) {
+            result.then(value => current.downstreamPromise._internalResolve(value),
+              err => current.downstreamPromise._internalReject(err));
+          } else {
+            current.downstreamPromise._internalResolve(result);
+          }
+        } catch (e) {
+          current.downstreamPromise._internalReject(e);
+        }
+      }
     }
   }
-}
+};
 
-$Promise.prototype.catch = function(errorCb){
+$Promise.prototype.catch = function (errorCb) {
   return this.then(null, errorCb);
 }
 
